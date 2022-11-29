e498da3f8f08eb5fb7acc7b23b16a831f9802fd9
Alexander Ukhanov <alexander.ukhanov@dualbootpartners.com>
2022-11-29 23:56:05 +0300

    fs_read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { existsSync } from 'fs';
+import { readFile } from 'fs/promises';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = 'src/fs/files/fileToRead.txt';
+    const isError = !existsSync(filePath);
+    try {
+        if(isError){
+            throw new Error('FS operation failed')
+        }
+        const fileContent = await readFile(filePath,{encoding: 'utf8'});
+        console.log(fileContent);
+    } catch(error) {
+        console.error(error);
+    }
 };
 
 await read();
