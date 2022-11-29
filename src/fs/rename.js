b89b3231dea0374f8751a05b3c817e66b9ecb439
Alexander Ukhanov <alexander.ukhanov@dualbootpartners.com>
2022-11-29 23:41:25 +0300

    fs_rename

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,18 @@
+import { existsSync } from 'fs';
+import { rename as fsRename } from 'fs/promises';
+
 const rename = async () => {
-    // Write your code here 
+    const wrongNameFilePath = 'src/fs/files/wrongFilename.txt';
+    const properNameFilePath = 'src/fs/files/properFilename.md';
+    const isError = !existsSync(wrongNameFilePath) || existsSync(properNameFilePath);
+    try {
+        if (isError){
+            throw new Error('FS operation failed')
+        }
+        await fsRename(wrongNameFilePath,properNameFilePath);
+    } catch(error) {
+        console.error(error)
+    }
 };
 
 await rename();
