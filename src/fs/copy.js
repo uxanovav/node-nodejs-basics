48f2fbb5126bcd7aecc1763e5ea61d2a151bdc21
Alexander Ukhanov <alexander.ukhanov@dualbootpartners.com>
2022-11-29 23:32:23 +0300

    fs_copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,22 @@
+import { existsSync } from 'fs';
+import { cp, readdir } from 'fs/promises';
+
 const copy = async () => {
-    // Write your code here 
+   const folderPath = 'src/fs/files';
+   const copyFolderPath = 'src/fs/files_copy';
+   const isError = !existsSync(folderPath) || existsSync(copyFolderPath);
+   try {
+    if (isError){
+        throw new Error('FS operation failed');
+    }
+    const fileNames = await readdir(folderPath)
+    const promises = fileNames.map((fileName) => {
+       return cp(`${folderPath}/${fileName}`,`${copyFolderPath}/${fileName}`);
+    })
+    await Promise.all(promises);
+   } catch (error) {
+    console.error(error);
+   }
 };
 
 copy();
