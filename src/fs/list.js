323acbdba079f459a10329ff369ba495cf4746ab
Alexander Ukhanov <alexander.ukhanov@dualbootpartners.com>
2022-11-29 23:49:50 +0300

    fs_list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,18 @@
+import { existsSync } from 'fs';
+import { readdir } from 'fs/promises';
+
 const list = async () => {
-    // Write your code here 
+    const folderPath = 'src/fs/files';
+    const isError = !existsSync(folderPath)
+    try {
+        if(isError){
+            throw new Error('FS operation failed')
+        }
+        const fileNames = await readdir(folderPath);
+        fileNames.forEach(fileName => console.log(fileName));
+    } catch (error){
+        console.error(error);
+    }
 };
 
 await list();
