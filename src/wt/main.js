a886dddde2004852089e7a399d3362cde0498e77
Alexander Ukhanov <alexander.ukhanov@dualbootpartners.com>
2022-12-04 21:24:10 +0300

    wt_worker_threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,27 @@
+import {Worker} from 'worker_threads';
+import {cpus} from 'os';
+
 const performCalculations = async () => {
-    // Write your code here
+    const cores = cpus();
+    const workerPath = './src/wt/worker.js';
+    let result = [];
+
+    const promises = cores.map( async (_,index) => {
+        return new Promise((resolve, reject) => {
+            const worker = new Worker(workerPath, {
+                workerData: 10 + index
+            })
+            worker.on('message', resolve);
+            worker.on('error', reject);
+        }).then(value => result.push(value));
+    })
+
+    try {
+        await Promise.all(promises);
+        console.log(result.sort((a,b)=> a.data - b.data));
+    } catch(error) {
+        console.error(error);
+    }
 };
 
 await performCalculations();
--- src/wt/worker.js
+++ src/wt/worker.js
@@ -1,8 +1,16 @@
-// n should be received from main thread
+import {parentPort, workerData} from 'worker_threads';
+
 const nthFibonacci = (n) => n < 2 ? n : nthFibonacci(n - 1) + nthFibonacci(n - 2);
 
 const sendResult = () => {
-    // This function sends result of nthFibonacci computations to main thread
+    let result;
+
+    try {
+        result = {status: 'resolved', data: nthFibonacci(workerData)}
+    } catch {
+        result = {status: 'error', data: null}
+    }
+    parentPort.postMessage(result);
 };
 
 sendResult();
