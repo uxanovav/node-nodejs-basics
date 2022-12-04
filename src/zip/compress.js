c08b063719356b4a9cbd27e69f89a0663ffd8a3c
Alexander Ukhanov <alexander.ukhanov@dualbootpartners.com>
2022-12-04 19:48:26 +0300

    zip_compress_decompress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,14 @@
+import {deflate} from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const compress = async () => {
-    // Write your code here 
+    const filePath = 'src/zip/files/fileToCompress.txt';
+    const compressedFilePath = 'src/zip/files/archive.gz'
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(compressedFilePath);
+    readStream.on('data', (data) => {
+        deflate(data, (_,result) => writeStream.write(result));
+    })
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import {inflate} from 'zlib';
+import { createReadStream, createWriteStream } from 'fs';
+
 const decompress = async () => {
-    // Write your code here 
+    const filePath = 'src/zip/files/fileToCompress.txt';
+    const compressedFilePath = 'src/zip/files/archive.gz'
+    const readStream = createReadStream(compressedFilePath);
+    const writeStream = createWriteStream(filePath);
+    readStream.on('data', (data) => {
+        inflate(data, (_,result) => writeStream.write(result));
+    })
 };
 
 await decompress();
